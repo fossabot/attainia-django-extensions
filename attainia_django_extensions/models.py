ab5d86c621960fa31c337f146d0c691ab3d40aca
Glenn Siegman <gsiegman@kbsp.com>
2017-10-22 21:23:44 -0700

    allow nulls for created and modified by

--- attainia_django_extensions/models.py
+++ attainia_django_extensions/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class AuditTrailModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    created_by = models.UUIDField(blank=True)
-    modified_by = models.UUIDField(blank=True)
+    created_by = models.UUIDField(blank=True, null=True)
+    modified_by = models.UUIDField(blank=True, null=True)
 
     class Meta:
         abstract = True
